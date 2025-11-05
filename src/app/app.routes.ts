93722d782206f0e61b957354be9824ff3ee77dc6
SINC\marco.doell <marco.doell@topdev.de>
2025-11-05 16:06:05 +0100

    xx

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
-import { BookListComponent } from './books/book-list.component';
-import { BookDetailsComponent } from './books/book-details.component';
-import { BookCreateComponent } from './books/book-create.component';
-import { KanbanBoardComponent } from './books/kanban-board.component';
 
 export const routes: Routes = [
-  { path: '', component: BookListComponent },
-  { path: 'kanban', component: KanbanBoardComponent },
-  { path: 'books/new', component: BookCreateComponent },
-  { path: 'books/:isbn', component: BookDetailsComponent },
+  {
+    path: '',
+    loadComponent: () => import('./books/book-list.component').then(m => m.BookListComponent)
+  },
+  {
+    path: 'kanban',
+    loadComponent: () => import('./books/kanban-board.component').then(m => m.KanbanBoardComponent)
+  },
+  {
+    path: 'books/new',
+    loadComponent: () => import('./books/book-create.component').then(m => m.BookCreateComponent)
+  },
+  {
+    path: 'books/:isbn',
+    loadComponent: () => import('./books/book-details.component').then(m => m.BookDetailsComponent)
+  },
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/components/user-profile.ts
@@ -0,0 +1,16 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  imports: [],
+  template: `
+    <p>
+      user-profile works!
+    </p>
+  `,
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserProfile {
+
+}
